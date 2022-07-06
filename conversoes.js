4d7f9673a307c42fbf19338a316d3e5c202fb761
MrVini98 <vinisalvador11@gmail.com>
2022-07-06 18:56:35 -0300

    codigos var e conversões

---
+++ conversoes.js
@@ -0,0 +1,15 @@
+//tipo de dado 
+//booleano
+
+//conversõa implicita
+const numero = 915;
+const numeroString = '915';
+
+
+//Number()
+//String()
+console.log(numeroString + numero)
+
+
+//conversão explicita
+console.log(Number(numeroString) + numero)
