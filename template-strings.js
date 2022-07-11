ca8c89ca35e44d9b5b16f549805e6c474fcbddb2
MrVini98 <vinisalvador11@gmail.com>
2022-07-11 19:33:10 -0300

    strings e comparações

---
+++ template-strings.js
@@ -0,0 +1,12 @@
+const nome = 'Vini';
+const idade = 2022 - 2001;
+const cidadeDeNascimento = 'Taboão';
+// const apresentacao = 'Meu nome é ' + nome + ', minha idade é ' + idade + ' e nasci em ' + cidadeDeNascimento;
+
+
+// ``
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+console.log(apresentacao)
+
+
+
